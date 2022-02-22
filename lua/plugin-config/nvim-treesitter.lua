a6a4851c225d43f8fa1c16a043bf126105d51676
xiaolitongxue666 <leonpa1987@gmail.com>
2022-02-22 14:58:43 +0000

    fix error push

--- lua/plugin-config/nvim-treesitter.lua
+++ lua/plugin-config/nvim-treesitter.lua
@@ -1,7 +1,7 @@
 require'nvim-treesitter.configs'.setup {
   -- 安装 language parser
   -- :TSInstallInfo 命令查看支持的语言
-  ensure_installed = {"bash", "c", "lua", "rust", "vim"},
+  ensure_installed = {"bash", "c", "lua", "rust", "vim", "python", "html"},
   -- 启用代码高亮功能
   highlight = {
     enable = true,
