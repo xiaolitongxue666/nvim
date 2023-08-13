eaa726ccfb41a005b289aeda17b552ac5b2e4d9e
XiaoLi <leonpa1987@gmail.com>
2023-08-13 16:41:58 +0800

    Update plugs

--- lua/plugins/code_highlight_nvim-treesitter.lua
+++ lua/plugins/code_highlight_nvim-treesitter.lua
@@ -1,10 +1,60 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        opts = function(_, opts)
+        build = ":TSUpdate",
+        event = { "BufReadPost", "BufNewFile" },
+        cmd = { "TSUpdateSync" },
+        keys = {
+            { "<c-space>", desc = "Increment selection" },
+            { "<bs>", desc = "Decrement selection", mode = "x" },
+        },
+        opts = {
+            highlight = { enable = true },
+            indent = { enable = true },
+            ensure_installed = {
+                "bash",
+                "c",
+                "html",
+                "javascript",
+                "jsdoc",
+                "json",
+                "lua",
+                "luadoc",
+                "luap",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "regex",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "yaml",
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+        },
+        config = function(_, opts)
             if type(opts.ensure_installed) == "table" then
-                vim.list_extend(opts.ensure_installed, { "c", "cpp" })
+                ---@type table<string, boolean>
+                local added = {}
+                opts.ensure_installed = vim.tbl_filter(function(lang)
+                    if added[lang] then
+                        return false
+                    end
+                    added[lang] = true
+                    return true
+                end, opts.ensure_installed)
             end
+            require("nvim-treesitter.configs").setup(opts)
         end,
     },
 }
---
+++ lua/plugins/search_and_replace_nvim-spectre.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "nvim-pack/nvim-spectre",
+        cmd = "Spectre",
+        opts = { open_cmd = "noswapfile vnew" },
+        -- stylua: ignore
+        keys = {
+            { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
+        },
+    },
+}
