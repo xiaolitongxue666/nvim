794cb09d609f44d3bf8f7c3f8e6342183c0a3147
XiaoLi <leonpa1987@gmail.com>
2023-08-13 17:18:21 +0800

    Update session save

--- lua/plugins/session_manager_persistence.lua
+++ lua/plugins/session_manager_persistence.lua
@@ -5,5 +5,12 @@ return {
         config = function()
             require("persistence").setup()
         end,
+        opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
+        -- stylua: ignore
+        keys = {
+            { "<leader>ws", function() require("persistence").load() end, desc = "Restore Session" },
+            { "<leader>wl", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+            { "<leader>wd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+        },
     },
 }
