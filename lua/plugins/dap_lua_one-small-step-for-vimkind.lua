22391378c27123dd56ae17fea1be74578efb633f
XiaoLi <leonpa1987@gmail.com>
2023-08-22 00:30:10 +0800

    update

---
+++ lua/plugins/dap_lua_one-small-step-for-vimkind.lua
@@ -0,0 +1,35 @@
+-- jbyuki/one-small-step-for-vimkind
+
+-- Debug adapter for Neovim plugins
+
+-- https://github.com/jbyuki/one-small-step-for-vimkind
+
+return {
+    -- Plug name
+    "jbyuki/one-small-step-for-vimkind",
+    -- A list of plugin names or plugin specs that should be loaded when the plugin loads.
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+    },
+    -- Config is executed when the plugin loads.
+    config = function()
+        local dap = require("dap")
+        dap.adapters.nlua = function(callback, config)
+            callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
+        end
+        dap.configurations.lua = {
+            {
+                type = "nlua",
+                request = "attach",
+                name = "Attach to running Neovim instance",
+            },
+        }
+    end,
+    -- Lazy-load on key mapping
+    keys = {
+        { "<leader>daL", function() require("osv").launch({ port = 8086 }) end, desc = "Adapter Lua Server" },
+        { "<leader>dal", function() require("osv").run_this() end, desc = "Adapter Lua" },
+    },
+}
+
---
+++ lua/plugins/dap_nvim-dap.lua
@@ -0,0 +1,125 @@
+-- mfussenegger/nvim-dap
+
+-- Debug Adapter Protocol client implementation for Neovim
+
+-- https://github.com/mfussenegger/nvim-dap
+
+return {
+    {
+        -- Plug name
+        "mfussenegger/nvim-dap",
+        -- A list of plugin names or plugin specs that should be loaded when the plugin loads.
+        dependencies = {
+            -- fancy UI for the debugger
+            {
+                -- Dependency plug name
+                "rcarriga/nvim-dap-ui",
+                -- Lazy-load on key mapping
+                keys = {
+                    { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+                    { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+                },
+                -- Opts is a table will be passed to the Plugin.config() function. Setting this value will imply Plugin.config()
+                opts = {},
+                -- Config is executed when the plugin loads.
+                config = function(_, opts)
+                    local dap = require("dap")
+                    local dapui = require("dapui")
+                    dapui.setup(opts)
+                    dap.listeners.after.event_initialized["dapui_config"] = function()
+                        dapui.open({})
+                    end
+                    dap.listeners.before.event_terminated["dapui_config"] = function()
+                        dapui.close({})
+                    end
+                    dap.listeners.before.event_exited["dapui_config"] = function()
+                        dapui.close({})
+                    end
+                end,
+            },
+
+            -- virtual text for the debugger
+            {
+                -- Dependency plug name
+                "theHamsta/nvim-dap-virtual-text",
+                -- Opts is a table will be passed to the Plugin.config() function. Setting this value will imply Plugin.config()
+                opts = {},
+            },
+
+            -- which key integration
+            {
+                -- Dependency plug name
+                "folke/which-key.nvim",
+                -- This is mainly useful for Neovim distros, to allow setting options on plugins that may/may not be part of the user's plugins
+                optional = true,
+                -- Opts is a table will be passed to the Plugin.config() function. Setting this value will imply Plugin.config()
+                opts = {
+                    defaults = {
+                        ["<leader>d"] = { name = "+debug" },
+                        ["<leader>da"] = { name = "+adapters" },
+                    },
+                },
+            },
+
+            -- mason.nvim integration
+            {
+                -- Dependency plug name
+                "jay-babu/mason-nvim-dap.nvim",
+                -- A list of plugin names or plugin specs that should be loaded when the plugin loads.
+                dependencies = "mason.nvim",
+                -- Lazy-load on command
+                cmd = { "DapInstall", "DapUninstall" },
+                -- Opts is a table will be passed to the Plugin.config() function. Setting this value will imply Plugin.config()
+                opts = {
+                    -- Makes a best effort to setup the various debuggers with
+                    -- reasonable debug configurations
+                    automatic_installation = true,
+
+                    -- You can provide additional configuration to the handlers,
+                    -- see mason-nvim-dap README for more information
+                    handlers = {},
+
+                    -- You'll need to check that you have the required things installed
+                    -- online, please don't ask me how to install them :)
+                    ensure_installed = {
+                        -- Update this to ensure that you have the debuggers for the langs you want
+                    },
+                },
+            },
+        },
+
+        -- Lazy-load on key mapping
+        keys = {
+            { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+            { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+            { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+            { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+            { "<leader>dg", function() require("dap").goto_() end, desc = "Go to line (no execute)" },
+            { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+            { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+            { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+            { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+            { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+            { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+            { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+            { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+            { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+            { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+            { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+        },
+
+        ---- Config is executed when the plugin loads.
+        --config = function()
+        --    local Config = require("lazyvim.config")
+        --    vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+        --
+        --    for name, sign in pairs(Config.icons.dap) do
+        --        sign = type(sign) == "table" and sign or { sign }
+        --        vim.fn.sign_define(
+        --                "Dap" .. name,
+        --                { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
+        --        )
+        --    end
+        --end,
+    },
+}
